805db76d4b1db4db946b89ca6c444591d3da2cbb
do-dorio <zikokeno@gmail.com>
2025-06-03 14:34:38 +0900

    build: include generated routes.js for Vercel

---
+++ assets/build/routes.js
@@ -0,0 +1,22 @@
+export default {
+    comment2434: {
+        routes: {
+            '/:keyword': {
+                path: '/:keyword',
+                categories: ['community'],
+                example: '/comment2434/猫',
+                parameters: {
+                    keyword: '検索キーワード（例：「猫」や「ゲーム」など）',
+                },
+                name: 'Comment2434',
+                url: 'comment2434.com',
+                maintainers: ['yourGitHubUsername'],
+                location: 'router.ts',
+                module: () => import('@/routes/comment2434/router.ts'),
+            },
+        },
+        name: 'comment2434',
+        url: 'https://comment2434.com/',
+        description: 'comment2434 の RSS フィード',
+    },
+};
